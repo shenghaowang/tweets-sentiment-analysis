d5b46ab1a505d1db002a864bae978c3a2178507f
Shenghao Wang <wangshenghao1993@gmail.com>
2025-03-06 18:10:30 -0500

    Create dataclass for tweet columns

---
+++ src/preprocess/tweet_columns.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class TweetColumns:
+    post_id: str = "post_id"
+    user_id: str = "user_id"
+    created_at: str = "created_at"
+    text: str = "text"
+    retweet_count: str = "retweet_count"
+    favorite_count: str = "favorite_count"
+    reply_count: str = "reply_count"
+    quote_count: str = "quote_count"
+    hashtags: str = "hashtags"
+    location: str = "location"
+    place: str = "place"
