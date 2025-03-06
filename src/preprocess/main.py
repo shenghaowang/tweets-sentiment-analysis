14ed2c7587cc1e6a9968738959a9fd0fbd90f48a
Shenghao Wang <wangshenghao1993@gmail.com>
2025-03-06 18:11:29 -0500

    Process text and hashtags

--- src/preprocess/main.py
+++ src/preprocess/main.py
@@ -2,8 +2,12 @@ import json
 from pathlib import Path
 
 import hydra
+import pandas as pd
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
+
+from preprocess.tweet_columns import TweetColumns
+from preprocess.utils import process_tags, process_text
 
 
 @hydra.main(version_base=None, config_path="../../config", config_name="data")
@@ -21,6 +25,24 @@ def main(cfg: DictConfig) -> None:
             tweets.extend(data)
 
     logger.info(f"Number of tweets: {len(tweets)}")
+
+    # Convert tweets to dataframe
+    tweets_df = pd.DataFrame(tweets)
+
+    # Process text and hashtags
+    tweet_cols = TweetColumns()
+    tweets_df[tweet_cols.text] = tweets_df[tweet_cols.text].apply(process_text)
+    tweets_df[tweet_cols.hashtags] = tweets_df[tweet_cols.hashtags].apply(process_tags)
+    logger.debug(f"\n{tweets_df.head()}")
+
+    # Remove duplicate tweets
+    tweets_df = tweets_df.drop_duplicates(
+        subset=[tweet_cols.post_id, tweet_cols.user_id], keep="first"
+    )
+
+    # Write processed tweets to file
+    tweets_df.to_csv(cfg.processed_filepath, index=False)
+    logger.info(f"{tweets_df.shape[0]} tweets written to {cfg.processed_filepath}")
 
 
 if __name__ == "__main__":
