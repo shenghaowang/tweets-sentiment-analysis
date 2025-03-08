0f6ec434812988ba31370fc13b9788742c5e6fe8
Shenghao Wang <wangshenghao1993@gmail.com>
2025-03-08 11:13:45 -0500

    Move handle missing value to other module

--- src/preprocess/main.py
+++ src/preprocess/main.py
@@ -35,11 +35,6 @@ def main(cfg: DictConfig) -> None:
     tweets_df[tweet_cols.hashtags] = tweets_df[tweet_cols.hashtags].apply(process_tags)
     tweets_df[tweet_cols.location] = tweets_df[tweet_cols.location].apply(process_text)
 
-    # Handle missing columns
-    for col in (tweet_cols.text, tweet_cols.location, tweet_cols.hashtags):
-        tweets_df[col] = tweets_df[col].fillna("")
-        tweets_df[col] = tweets_df[col].astype(str)
-
     logger.debug(f"\n{tweets_df.head()}")
 
     # Remove duplicate tweets
