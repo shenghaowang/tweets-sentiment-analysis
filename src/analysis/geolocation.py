702ca40fd5f25743490654f1e81cde0227ec0c94
Shenghao Wang <wangshenghao1993@gmail.com>
2025-03-08 10:58:06 -0500

    Identify location from tweets

---
+++ src/analysis/geolocation.py
@@ -0,0 +1,13 @@
+import stanza
+
+stanza.download("en")
+
+# Load the English NLP pipeline with Named Entity Recognition (NER)
+nlp = stanza.Pipeline(lang="en", processors="tokenize,ner")
+
+
+def identify_location(text: str) -> str:
+    doc = nlp(text)
+    locations = [ent.text for ent in doc.entities if ent.type == "GPE"]
+
+    return locations[0] if locations else None
--- src/analysis/main.py
+++ src/analysis/main.py
@@ -2,9 +2,13 @@ import hydra
 import pandas as pd
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
+from tqdm import tqdm
 
 from analysis.classifier import TweetClassifier
+from analysis.geolocation import identify_location
 from preprocess.tweet_columns import TweetColumns
+
+tqdm.pandas()
 
 
 @hydra.main(version_base=None, config_path="../../config", config_name="config")
@@ -14,32 +18,42 @@ def main(cfg: DictConfig) -> None:
     # Load processed tweets
     tweets_df = pd.read_csv(cfg.processed_filepath)
 
-    # Handle missing text
-    tweet_cols = TweetColumns()
-    tweets_df[tweet_cols.text] = tweets_df[tweet_cols.text].fillna("")
-    tweets_df[tweet_cols.text] = tweets_df[tweet_cols.text].astype(str)
-
-    # Classify sentiment
+    # Classify by sentiment
     classifier = TweetClassifier()
-    tweets_df[tweet_cols.sentiment] = tweets_df[tweet_cols.text].apply(
+    tweet_cols = TweetColumns()
+    tweets_df[tweet_cols.sentiment] = tweets_df[tweet_cols.text].progress_apply(
         classifier.classify_sentiment
     )
-    tweets_df[tweet_cols.risk_level] = tweets_df[tweet_cols.text].apply(
-        classifier.classify_risk
-    )
-
     logger.info(
         f"Distribution of posts by sentiment: \n{tweets_df[tweet_cols.sentiment].value_counts()}"
+    )
+    tweets_df[tweet_cols.sentiment].value_counts().to_csv(
+        cfg.distribution_filepath.sentiment, index=True, header=True
+    )
+
+    # Classify by risk level
+    tweets_df[tweet_cols.risk_level] = tweets_df[tweet_cols.text].progress_apply(
+        classifier.classify_risk
     )
     logger.info(
         f"Distribution of posts by risk level: \n{tweets_df[tweet_cols.risk_level].value_counts()}"
     )
-
-    tweets_df[tweet_cols.sentiment].value_counts().to_csv(
-        cfg.distribution_filepath.sentiment, index=True, header=True
-    )
     tweets_df[tweet_cols.risk_level].value_counts().to_csv(
         cfg.distribution_filepath.risk_level, index=True, header=True
+    )
+
+    # Classify by location
+    tweets_df[tweet_cols.valid_location] = tweets_df.progress_apply(
+        lambda row: identify_location(
+            row[tweet_cols.location] + row[tweet_cols.hashtags] + row[tweet_cols.text]
+        ),
+        axis=1,
+    )
+    logger.info(
+        f"Distribution of posts by valid location: \n{tweets_df[tweet_cols.valid_location].value_counts()}"
+    )
+    tweets_df[tweet_cols.valid_location].value_counts().to_csv(
+        cfg.distribution_filepath.location, index=True, header=True
     )
 
 
